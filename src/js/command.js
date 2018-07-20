58e42e0044ba3af31b39949ae431769c93ea01dd
JIZHUA <jizhua@fenjiandan.com>
2018-07-20 11:18:50 +0800

    step4

--- src/js/command.js
+++ src/js/command.js
@@ -6,19 +6,34 @@
 
 
 (function () {
+  var lastEditRange = null
 
   var editorEle = document.getElementById('editor')
+  editorEle.addEventListener('keyup', function() {
+    lastEditRange  = window.getSelection().getRangeAt(0)
+  })
 
   var toBoldEle = document.getElementById('bold')
   var boldToggle = false
   toBoldEle.addEventListener('click', function () {
+    setFocus(editorEle, lastEditRange)
     boldToggle = !boldToggle
     document.execCommand('bold', boldToggle, null)
   })
 
   var blockquoteEle = document.getElementById('blockquote')
   blockquoteEle.addEventListener('click', function() {
-    editorEle.focus()
+    setFocus(editorEle, lastEditRange)
+    console.log(window.getSelection().getRangeAt(0))
     document.execCommand('formatBlock', true, 'blockquote')
   })
 })()
+
+function setFocus(ele, range) {
+  ele.focus()
+  var selection = window.getSelection()
+  if (range) {
+    selection.removeAllRanges()
+    selection.addRange(range)
+  }
+}
